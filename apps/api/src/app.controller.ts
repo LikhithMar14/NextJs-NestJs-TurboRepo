80d6f4487b06be47f361b6afb63b42828b4f9ece
2005LIKHITH <likhithcvsrl@gmail.com>
2025-02-20 21:44:38 +0530

    Configured some rate-limiting issues related to package version

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from './auth/decorator/public.decorator';
+import { SkipThrottle, Throttle } from '@nestjs/throttler';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
   @Public()
   @Get()
+  @SkipThrottle({short:true})
+  // @Throttle({ default: { limit: 3, ttl: 60000 } })
   getHello(): string {
     return this.appService.getHello();
   }
--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,17 +5,32 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import { ThrottlerModule, ThrottlerGuard} from "@nestjs/Throttler"
+import { ThrottlerModule, ThrottlerGuard, seconds, minutes } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 @Module({
   imports: [AuthModule,
      UserModule,
      ConfigModule.forRoot({isGlobal:true}),
      PrismaModule,
-     ThrottlerModule.forRoot([{
-      ttl: 60000,
+    ThrottlerModule.forRoot({throttlers:[        {
+      name: 'short',
+      ttl: seconds(4),
+      limit: 3,
+      blockDuration: minutes(10),
+    },
+    {
+      name: 'medium',
+      ttl: seconds(30),
       limit: 10,
-    }]),
+    },
+    {
+      name: 'long',
+      ttl: seconds(60),
+      limit: 20,
+    },
+  ],
+  errorMessage: 'Wow! Slow down.',
+  }),
   ],
   controllers: [AppController],
   providers: [AppService,
